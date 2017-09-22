40238e9257c673e70f32016cfeaa07f62e26f646
Sahana Balram <sahana2008balram@gmail.com>
2017-09-22 16:37:12 -0700

    Fix styling

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friendArray = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        "name": "Shia",
+        "photo": "https://media.gq.com/photos/58488eaebe0abd2458a0d46f/master/w_4200/shia-labeouf-hair.jpg",
+        "scores": [
+            3,
+            2,
+            4,
+            1,
+            2,
+            2,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    },
+
+    {
+        "name": "Jennifer Aniston",
+        "photo": "https://media.gq.com/photos/58488eaebe0abd2458a0d46f/master/w_4200/shia-labeouf-hair.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+
+    },
+
+]
+module.exports = friendArray;
