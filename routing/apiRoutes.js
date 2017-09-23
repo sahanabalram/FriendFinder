9d8dd590bcb4e4a6b5dc99682d32015da8f22809
Sahana Balram <sahana2008balram@gmail.com>
2017-09-23 11:51:14 -0700

    Fix the comparasion code and display modal

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,11 +3,12 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-var friendsData = require("./../app/data/friends.js");
-
+var fs = require("fs");
+var path = require("path");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
+var fname = path.join(__dirname, "../app/data/friends.js");
 
 module.exports = function (app) {
     // API GET Requests
@@ -15,40 +16,61 @@ module.exports = function (app) {
     // In each of the below cases when a user visits a link
     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
+    // "/Users/sahana/Desktop/BootCamp/HomeWork/week7/FriendFinder/app/data/friends.js"
+
+    app.get("/api/friends", function (request, response) {
+        var rawdata = fs.readFileSync(fname);
+        var friendsData = JSON.parse(rawdata);
+        response.json(friendsData);
     });
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
     // In each of the below cases, when a user submits form data (a JSON object)
     // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
+    // (ex. User fills out a survey ...
+    // Once saved, the server should acknowledge the client with a response
+    // The response can be as simple as "I have received your data, thank you"
+    // In this specific case, we need to save the data and then return the
+    // compatible friend  with a friendData with the  that has been received
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function (req, res) {
         var friend = {
-            scores: req.body.scores,
             name: req.body.name,
-            photo: req.body.photo
+            photo: req.body.photo,
+            scores: req.body.scores
         };
+        // data is received in a raw format and then in the next line the raw data is converted into an  JSON object
+        var rawdata = fs.readFileSync(fname);
+        var friendsData = JSON.parse(rawdata);
         // index of highest value in compare array and get the index of the friend in the friend array
         var compare = [];
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
+        // Note the code here. Our "server" will respond to requests and let users know if they have a friend compatible
+        // It will do this by sending out the value "true" have a friend
         for (var i = 0; i < friendsData.length; i++) {
             // resets the totalDifference to 0 everytime the loop runs and then pushes the number into the array
             var totalDifference = 0;
             for (var j = 0; j < friend.scores.length; j++) {
-                var userInputDifference = friendsData[i].scores[j] - friend.scores[j];
+                var userInputDifference = parseInt(friendsData[i].scores[j]) - friend.scores[j];
                 totalDifference += Math.abs(userInputDifference);
                 // index of surveryResult j is the index and surveryResult[j] is the value at that index
                 if (j === friend.scores.length - 1) {
-                    compare.push(totalDifference);
+                    
                 }
             }
+            compare.push(totalDifference);
         }
-        console.log(compare);
+        var minimum = compare[0];
+        // Trying to find the index of the least difference between the userInput and the existing data. 
+        // The loop is used to find the minimun value in a array
+        for (var k = 0; k < compare.length; k++) {
+            if (compare[k] < minimum) {
+                minimum = compare[k];
+            }
+        }
+        friendsData.push(friend);
+        fs.writeFileSync(fname, JSON.stringify(friendsData));
+        res.json(friendsData[compare.indexOf(minimum)]);
     });
 
     // ---------------------------------------------------------------------------
